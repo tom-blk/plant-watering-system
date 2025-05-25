1d768619bd8ad6fb4d710e69b6030a24a37a61a9
Tom Bleek <tom.bleek@proton.me>
2025-05-25 21:36:28 +0200

    Init server app and controller

---
+++ server/target/debug/deps/server-09373dae7d27192a.d
@@ -0,0 +1,5 @@
+/home/tom/plant-watering-system/server/target/debug/deps/server-09373dae7d27192a: src/main.rs
+
+/home/tom/plant-watering-system/server/target/debug/deps/server-09373dae7d27192a.d: src/main.rs
+
+src/main.rs:
---
+++ server/target/debug/server.d
@@ -0,0 +1,1 @@
+/home/tom/plant-watering-system/server/target/debug/server: /home/tom/plant-watering-system/server/src/main.rs
