c0f190105071f0e91f50e97dedbacc6a65771cc4
Tom Bleek <tom.bleek@proton.me>
2025-05-25 16:59:35 +0200

    Init

---
+++ main.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+import time
+
+PUMP_PIN = 17
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PUMP_PIN, GPIO.OUT)
+
+GPIO.output(PUMP_PIN, GPIO.HIGH)
+time.sleep(5)
+GPIO.output(PUMP_PIN, GPIO.LOW)
+
+GPIO.cleanup()
